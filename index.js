1111b166bd2744892062c590941867056acf1ae4
Riley Rangel <rrangel91@gmail.com>
2017-10-08 15:11:11 -0700

    Install body parser as a dependency. #1

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const { MongoClient } = require('mongodb')
 const path = require('path')
 const uuidv4 = require('uuid/v4')
+const bodyParser = require('bodyParser')
 const app = express()
 
 const publicPath = path.join(__dirname, 'public')
 app.use(express.static(publicPath))
+app.use(bodyParser)
 
 app.post('/', (req, res) => {
   MongoClient.connect('mongodb://localhost/notepad', (error, db) => {
     if (error) {
       console.error(error)
+      res.sendStatus(404)
       process.exit(1)
     }
     const notes = db.collection('notes')
