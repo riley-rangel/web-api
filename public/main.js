d349c838647e02317c8c0c512825117206a4cd12
Riley Rangel <rrangel91@gmail.com>
2017-10-08 11:51:17 -0700

    Add render function to show field and button for user's use. #1

---
+++ public/main.js
@@ -0,0 +1,23 @@
+function createElement(tag, attributes, children) {
+  const $element = document.createElement(tag)
+  for (let key in attributes) {
+    $element.setAttribute(key, attributes[key])
+  }
+  children.forEach(child => {
+    child instanceof Node
+      ? $element.appendChild(child)
+      : $element.appendChild(document.createTextNode(child))
+  })
+  return $element
+}
+
+function renderAddNote() {
+  const $addNote = createElement('div', {'class': 'main view'}, [
+    createElement('h1', {}, ['Add a Note']),
+    createElement('input', {'type': 'text', 'value': 'Type note here'}, []),
+    createElement('button', {}, ['Add Note'])
+  ])
+  return $addNote
+}
+
+document.body.appendChild(renderAddNote())
