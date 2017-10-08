705a1e1d19daf530b2896023753ae9a57aa7e9f5
Riley Rangel <rrangel91@gmail.com>
2017-10-08 13:21:45 -0700

    Set up key-value pairs of object to be added on user 'save'. #1

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const { MongoClient } = require('mongodb')
 const path = require('path')
+const uuidv4 = require('uuid/v4')
 const app = express()
 
 const publicPath = path.join(__dirname, 'public')
 app.use(express.static(publicPath))
 
-app.post('/notes', (req, res) => {
+app.post('/', (req, res) => {
   MongoClient.connect('mongodb://localhost/notepad', (error, db) => {
     if (error) {
       console.error(error)
       process.exit(1)
     }
     const notes = db.collection('notes')
-    notes.insertOne({})
+    notes.insertOne({
+      '_id': uuidv4(),
+      'date': new Date(),
+      'user': 'user',
+      'content': ''
+    })
       .catch(reject => {
         console.error(reject)
         res.sendStatus(404)
