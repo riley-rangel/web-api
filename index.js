4848f9ba7409cb5a7ebdbca01bb7de019a0a7877
Riley Rangel <rrangel91@gmail.com>
2017-10-08 09:58:16 -0700

    Add connect method with url to new db: notes. #1

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
 
-app.post('mongodb://localhost/notes', () => {
-
+MongoClient.connect('mongodb://localhost/notes', (error, db) => {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+  app.post('/', (req, res) => {
+    res.send('connection OK')
+  })
+  db.close()
 })
 
 app.listen('3000', () => console.log('Port 3000 Open.'))
