d951960b43c1ba7960ed1850ab8e8b405379d033
Riley Rangel <rrangel91@gmail.com>
2017-10-08 10:33:31 -0700

    Add insertOne Method to add note. #1

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
 
-MongoClient.connect('mongodb://localhost/notes', (error, db) => {
-  if (error) {
-    console.error(error)
-    process.exit(1)
-  }
-  app.post('/', (req, res) => {
-    res.send('connection OK')
+app.post('/notes', (req, res) => {
+  MongoClient.connect('mongodb://localhost/notepad', (error, db) => {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    }
+    const notes = db.collection('notes')
+    notes.insertOne({})
+      .catch(reject => {
+        console.error(reject)
+        res.sendStatus(404)
+      })
+      .then(() => {
+        res.sendStatus(201)
+        db.close()
+      })
   })
-  db.close()
 })
 
 app.listen('3000', () => console.log('Port 3000 Open.'))
